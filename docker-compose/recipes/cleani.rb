922d2918b734b897b3b079139d84569cec58bd0b
Eugene <rampire@gmail.com>
2016-11-01 09:54:09 +0200

    Clean untagged images only if there are any.

--- docker-compose/recipes/cleani.rb
+++ docker-compose/recipes/cleani.rb
@@ -1,4 +1,5 @@
 execute 'clean untagged images' do
     cwd '/docker-compose/'
     command '/usr/bin/docker rmi $(/usr/bin/docker images -q -f dangling=true)'
+    only_if '/usr/bin/docker images -q -f dangling=true'
 end
