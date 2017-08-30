bbaf3584af75861abcc48a433e75c8f01408b901
Maxim Nasonov <maxim@nasonov.me>
2017-08-30 10:24:02 +0700

    Fix method invocation

--- prototype/recipes/default.rb
+++ prototype/recipes/default.rb
@@ -15,9 +15,9 @@ directory "#{Chef::Config['file_cache_path']}/mellon"
 
 node['prototype']['mellon_files'].each do |mellon_file|
   if mellon_file == "FederationMetadata.xml"
-    S3encrypt.getfile(mellon_file, "#{Chef::Config['file_cache_path']}/mellon/#{mellon_file}", node['prototype']['s3_bucket'], node['prototype']['encryption_context'])
+    S3encrypt.getfile("#{Chef::Config['file_cache_path']}/mellon/#{mellon_file}", mellon_file, node['prototype']['s3_bucket'], node['prototype']['encryption_context'])
   else
-    S3encrypt.getfile(mellon_file, "#{Chef::Config['file_cache_path']}/mellon/adfs.#{mellon_file.split('.')[-1]}", node['prototype']['s3_bucket'], node['prototype']['encryption_context'])
+    S3encrypt.getfile("#{Chef::Config['file_cache_path']}/mellon/adfs.#{mellon_file.split('.')[-1]}", mellon_file, node['prototype']['s3_bucket'], node['prototype']['encryption_context'])
   end
 end
 
