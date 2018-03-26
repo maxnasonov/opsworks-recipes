e834fbb9be3224218b7dbf3f4a3f6b8e77b3ab8d
Maxim Nasonov <maxim@nasonov.me>
2018-03-27 06:56:41 +0700

    Add td-agent cookbook

---
+++ chef-td-agent/metadata.rb
@@ -0,0 +1,19 @@
+name             "td-agent"
+maintainer       "Treasure Data, Inc."
+maintainer_email "k@treasure-data.com"
+license          "Apache 2.0"
+description      "Installs/Configures td-agent"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "3.0.2"
+recipe           "td-agent", "td-agent configuration"
+
+chef_version     ">= 12" if respond_to?(:chef_version)
+issues_url       "https://github.com/treasure-data/chef-td-agent/issues" if respond_to?(:issues_url)
+source_url       "https://github.com/treasure-data/chef-td-agent" if respond_to?(:source_url)
+
+%w{redhat centos debian ubuntu}.each do |os|
+  supports os
+end
+
+depends 'apt'
+depends 'yum'
