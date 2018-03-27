a6023d0e13f8233f5cf988ca9f4c12110ebe8b80
Maxim Nasonov <maxim@nasonov.me>
2018-03-27 09:33:12 +0700

    Fix service

--- prototype/recipes/default.rb
+++ prototype/recipes/default.rb
@@ -127,12 +127,6 @@ template "/lib/systemd/system/td-agent.service" do
   owner 'root'
   group 'root'
   mode '0644'
-  notifies :run, "execute[systemctl daemon-reload]", :immediately
-end
-
-execute 'systemctl daemon-reload' do
-  command 'systemctl daemon-reload'
-  action :nothing
   notifies :restart, "service[td-agent]", :delayed
 end
 
