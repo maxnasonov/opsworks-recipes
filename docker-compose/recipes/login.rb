a21be89ff3928a6f52a0e81fdfd6a3ae762967ae
Maxim Nasonov <maxim@nasonov.me>
2018-08-09 16:35:42 +0700

    Use ECR

--- docker-compose/recipes/login.rb
+++ docker-compose/recipes/login.rb
@@ -1,4 +1,11 @@
-execute "DockerHub login" do
-  action :run
-  command "/usr/bin/docker login -e #{node[:DockerHub][:login][:email]} -u #{node[:DockerHub][:login][:username]} -p #{node[:DockerHub][:login][:password]}"
+python_pip 'awscli' do
+  # Version that supports --no-include-email argument for aws ecr get-login
+  # It should be 1.11.91+ for Docker 17.05+
+  version '1.11.120'
+  action :install
+end
+
+execute "docker login" do
+  command "$(aws ecr get-login --no-include-email --region us-east-1)"
+  sensitive true
 end
