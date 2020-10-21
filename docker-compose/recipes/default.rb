9c64990ac62f9df678987eae62c9a1a7da902780
Max Nasonov <m@nasonov.me>
2020-10-22 02:11:31 +0700

    Use official docker

--- docker-compose/recipes/default.rb
+++ docker-compose/recipes/default.rb
@@ -1,10 +1,20 @@
+if node[:platform] == 'ubuntu'
+  apt_repository "docker" do
+    retries 3
+    uri "https://download.docker.com/linux/ubuntu"
+    distribution node['lsb']['codename']
+    components ["stable"]
+    keyserver "keyserver.ubuntu.com"
+    key "9DC858229FC7DD38854AE2D88D81803C0EBFCD88"
+  end
+end
 package 'Install Docker' do
   case node[:platform]
   when 'redhat', 'centos', 'amazon'
     package_name 'docker'
   when 'ubuntu', 'debian'
-    package_name 'docker.io'
-    version '1.6.2~dfsg1-1ubuntu4~14.04.1'
+    package_name 'docker-ce'
+    #version '1.6.2~dfsg1-1ubuntu4~14.04.1'
   end
 end
 
