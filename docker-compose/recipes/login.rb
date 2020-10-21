55565f9379eda1d4d8ee3b40a224d6eaeb09eab4
Max Nasonov <m@nasonov.me>
2020-10-22 05:00:39 +0700

    Update

--- docker-compose/recipes/login.rb
+++ docker-compose/recipes/login.rb
@@ -1,7 +1,7 @@
 python_pip 'awscli' do
   # Version that supports --no-include-email argument for aws ecr get-login
   # It should be 1.11.91+ for Docker 17.05+
-  version '1.11.120'
+  #version '1.11.120'
   action :install
 end
 
