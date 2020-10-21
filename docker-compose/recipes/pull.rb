94ae26c215bf4bc2bdbd41e45ed2cd2283e40529
Max Nasonov <m@nasonov.me>
2020-10-22 03:09:25 +0700

    Upgrade

--- docker-compose/recipes/pull.rb
+++ docker-compose/recipes/pull.rb
@@ -1,8 +1,8 @@
 execute 'pull containers images' do
     cwd '/docker-compose/'
     command 'docker-compose pull'
-    case node[:platform]
-    when 'ubuntu'
-      environment 'COMPOSE_API_VERSION' => '1.18'
-    end
+    #case node[:platform]
+    #when 'ubuntu'
+    #  environment 'COMPOSE_API_VERSION' => '1.18'
+    #end
 end
